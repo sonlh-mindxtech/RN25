4a84cd72db1883a7d1b53da1cd149b8bf5e7c1a5
Lưu Hoàng Sơn <sonlh@mindxtech.com>
2023-05-14 17:19:07 +0700

    init push - code for section 1

---
+++ revision/index.js
@@ -0,0 +1,87 @@
+
+// compile: biên dịch
+// inteprete: phiên dịch
+
+// variable & data type
+// primitive datatype.
+// 11234
+// "string"
+// true
+// false
+
+// non-primitive. Custom data type
+// [1,2,9]
+// {
+//     key: {
+//         key: [
+//             1,2,3
+//         ]
+//     }
+// }
+// class Point{
+//     x = 0
+//     y = 0
+//     Point(x,y){
+//         this.x = x
+//         this.y = y
+//     }
+// }
+// -- variable
+// naming convention
+// pascal case
+// let calculatedRevenueAndProfit
+// A->Z a->z 0 - 9 _ $
+// identifier: no start with number '0_abc'
+// no match with reserved keyword 'let'
+
+// -- control structure
+// branching
+// if else vs switch case
+// if (conditionExpression){
+//     if ()
+//     // do sth
+// }else if () {
+//     // do another thing
+// } else if (){
+
+// }
+// conditionExpression: true -> if-block
+//                        : false -> else-block
+// operator: + - * / ** -> operator precedence: like math ()
+// logic operator: && || ! -> operator precedence: not -> and -> or
+
+// loop
+// for - while - do/while
+// for( initialStatement ; loopCondition ;  ){
+//     // loop body
+//     // updateStatement
+// }
+// for (let i=0; 1 < 2 ; i++){
+//     console.log("abc")
+// }
+
+// for (let i=0; 1 < 2 ; i++){
+//     console.log("abc")
+// }
+// for (let i=0; 1 < 2 ; i++){
+//     console.log("abc")
+// }
+
+// -- function
+// function definition
+// function hello(country) {
+//     if (country == "vn"){
+//         console.log("Xin Chao")
+//     } else if (country == "en"){
+//         console.log("Hello")
+//     } else if (country == "")
+//     return
+// }
+
+// function invocation
+// let users = ['vn','en','jp']
+// for (let i =0;i < users.length; i++){
+//     let country = users[i];
+//     hello(country)
+// }
+//
