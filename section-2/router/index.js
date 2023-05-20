78572acb4442059a759e54c214bb5bf0fa3937cd
Lưu Hoàng Sơn <sonlh@mindxtech.com>
2023-05-20 12:09:39 +0700

    add instruction for homework

--- section-2/router/index.js
+++ section-2/router/index.js
@@ -5,6 +5,10 @@ import { randomCatHandler } from "../controller/randomCat.js"
 const baseRouter = Router()
 
 baseRouter.get('/random-cat/c',randomCatHandler)
+// BTVN
+// Viet api endpoint lấy random 1 tác giả từ api: https://lucifer-quotes.vercel.app/api/quotes
+// Các bước như randomCat
+// goi api -> edit -> send modified response to client
 baseRouter.get('/random-quote/c',randomQuoteHandler)
 
 export default baseRouter
