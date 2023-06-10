3f09319f187857b96ed18f188381d9f1df5f58d4
Lưu Hoàng Sơn <sonlh@mindxtech.com>
2023-06-10 14:46:15 +0700

    update section-3

---
+++ section-3/router/index.js
@@ -0,0 +1,28 @@
+import { Router } from "express"
+// Full URL
+import { randomCatHandler } from "../controller/randomCat.js"
+
+const baseRouter = Router()
+
+baseRouter.get('/random-cat/c',randomCatHandler)
+// BTVN
+// Viet api endpoint lấy random 1 tác giả từ api: https://lucifer-quotes.vercel.app/api/quotes
+// Các bước như randomCat
+// goi api -> edit -> send modified response to client
+// baseRouter.get('/random-quote/c',randomQuoteHandler)
+
+export default baseRouter
+
+
+// import { getPayment } from './../controller/payment'
+// import { Router } from "express"
+// import { paymentRouter } from './payment'
+// // Full URL
+
+// const baseRouter = Router()
+
+// baseRouter.get('/payment/:paymentId', getPayment)
+
+// baseRouter.use(paymentRouter)
+// export default baseRouter
+
