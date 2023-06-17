bfc5937e3e6f4f0ae6e0ea95996931b07a9a8b79
Lưu Hoàng Sơn <sonlh@mindxtech.com>
2023-06-17 16:23:51 +0700

    update section 4

---
+++ section-4/config/index.js
@@ -0,0 +1,6 @@
+const DB_CONN_STR = "mongodb+srv://sonluuhwf:26KvMme14JoKYWes@rn25.8d0rsed.mongodb.net"
+const DB_NAME = "RN25"
+const MERCHANT_COLLECTION_NAME = "merchants"
+const FOOD_COLLECTION_NAME = "foods"
+
+export { DB_CONN_STR, DB_NAME, MERCHANT_COLLECTION_NAME, FOOD_COLLECTION_NAME }
